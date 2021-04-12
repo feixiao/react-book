46c716ef405dee1fed91504460bd0c09e905c2e2
feixiao <feixiao2020@sina.com>
2021-04-12 10:05:53 +0800

    学习PropTypes

--- chapter-02/bbs-components-propTypes/src/PostItem.js
+++ chapter-02/bbs-components-propTypes/src/PostItem.js
@@ -26,6 +26,9 @@ function PostItem(props) {
   );
 }
 
+
+// 组件内部并没有明显地声明它暴露出哪些接口， 以及这些接口的类型是什么， 这不利于组件的复用
+// PropTypes包含组件属性所有可能的类型
 PostItem.propTypes = {
   post: PropTypes.shape({
     id: PropTypes.number,
@@ -34,7 +37,11 @@ PostItem.propTypes = {
     date: PropTypes.string,
     vote: PropTypes.number
   }).isRequired,
-  onVote: PropTypes.func.isRequired
+  onVote: PropTypes.func.isRequired   // 如果属性是组件的必需属性， 也就是当使用某个组件时， 必须传入的属性， 就需要在PropTypes的类型属性上调用isRequired
 }
+
+// 注意：
+// 当使用PropTypes.object或PropTypes.array校验属性类型时， 我们只知道这个属性是一个对象或一个数组， 至于对象的结构或数组元素的类型是什么样的， 依然无从得知。
+// 这种情况下， 更好的做法是使用PropTypes.shape或PropTypes.arrayOf
 
 export default PostItem;
