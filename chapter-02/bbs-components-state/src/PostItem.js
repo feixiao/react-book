d822b6a005f7dd2796644cbcb85c386799373015
feixiao <feixiao2020@sina.com>
2021-04-12 09:52:29 +0800

    学习react state使用

--- chapter-02/bbs-components-state/src/PostItem.js
+++ chapter-02/bbs-components-state/src/PostItem.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 
 class PostItem extends Component {
+  // constructor中通过this.state定义组件的初始状态，
+  // 并通过调用this.setState方法改变组件状态（也是改变组件状态的唯一方式），
+  // 进而组件UI也会随之重新渲染。
   constructor(props) {
     super(props);
+    // 组件的state是组件内部的状态， state的变化最终将反映到组件UI的变化上。
     this.state = {
       vote: 0
     };
@@ -40,6 +44,7 @@ class PostItem extends Component {
           </button>
           &nbsp;
           <span>
+            // 状态显示
             {this.state.vote}
           </span>
         </div>
